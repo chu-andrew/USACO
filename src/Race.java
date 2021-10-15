3386588a128cf0faf5afbfd0d8cf187a012a572d
chu-andrew <75499520+chu-andrew@users.noreply.github.com>
2021-10-15 01:21:07 -0700

    worked on race, unsolved

--- src/Race.java
+++ src/Race.java
@@ -1,12 +1,15 @@
 // http://www.usaco.org/index.php?page=viewproblem2&cpid=989:wq
+
+// unsolved
+
 import java.io.*;
 import java.util.Scanner;
 
 public class Race {
     public static void main(String[] args) throws IOException {
-        File in = new File("race.in");
+        File in = new File("src//race.in");
 
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("race.out")));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("src//race.out")));
 
         Scanner sc = new Scanner(in);
         int k = sc.nextInt();
@@ -14,7 +17,26 @@ public class Race {
 
         for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
+            out.println(race(k, x));
         }
+        sc.close();
+        out.close();
+    }
 
+    public static int race(int k, int x) {
+        // k is total length
+        // x is max final speed
+        int v = 0;
+        int s = 0;
+        for (int t = 0; t < 10000; t++) {
+            if (v < x || s + v + v + 1 < k) v++;
+            else if (s + v + v + 1 > k) v--;
+
+            s += v;
+            System.out.println("time:" + t + "velocity:" + v);
+
+            if (s > k) return t;
+        }
+        return -1;
     }
 }
